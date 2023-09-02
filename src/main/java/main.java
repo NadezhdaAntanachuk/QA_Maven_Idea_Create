350413104c0369f94f9774b3fc45b5716514cbb8
NadezhdaAntanachuk <8527132@mail.ru>
2023-09-02 20:05:42 +0300

    First commit

---
+++ src/main/java/main.java
@@ -0,0 +1,26 @@
+public class main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000;
+        boolean registered = true;
+        long expected = 30;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        System.out.println(expected + " ==?== " + actual);
+
+        amount = 1_000_000;
+        registered = true;
+        expected = 500;
+
+        // вызываем целевой метод:
+        actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        System.out.println(expected + " ==?== " + actual);
+    }
+}
